8987ac5b2dd97a48fe55b021a7520c0a02c0b224
zifrit <zifrit@list.ru>
2024-09-27 13:51:35 +0300

    change structure project

--- alembic/env.py
+++ alembic/env.py
@@ -7,8 +7,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from db.settings import settings
-from models.base_model import Base
+from src.db.settings import settings
+from src.models.base_model import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
