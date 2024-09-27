3323750f022787db22daa063c9f1d6bd61997ade
zifrit <zifrit@list.ru>
2024-09-27 17:13:25 +0300

    expanded the basic model, added created_at and updated_at

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -1,6 +1,19 @@
+from datetime import datetime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import DateTime, func
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
     id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(),
+        server_default=func.now(),
+        default=datetime.utcnow,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(),
+        server_default=func.now(),
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
