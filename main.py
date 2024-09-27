dad60eade1fa94484812f76abe50376a8cab644f
zifrit <zifrit@list.ru>
2024-09-27 18:23:20 +0300

    fixed main file and added logging config

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
+from src.utils.logging_setings import LOGGING
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        reload=True,
+        log_config=LOGGING,
+    )
