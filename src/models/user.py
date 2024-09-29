d9128749e817892b13bfa28c13f889a70a2abccf
zifrit <zifrit@list.ru>
2024-09-29 19:58:00 +0300

    change type password field str > bytes and set unique email field in users

--- src/models/user.py
+++ src/models/user.py
@@ -2,13 +2,14 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from .base_model import Base
 from sqlalchemy import String, Boolean
+from sqlalchemy.dialects.postgresql import BYTEA
 
 
 class Users(Base):
     __tablename__ = "users"
     username: Mapped[str] = mapped_column(String(255), unique=True)
-    email: Mapped[str] = mapped_column(String(255))
-    password: Mapped[str] = mapped_column(String(255))
+    email: Mapped[str] = mapped_column(String(255), unique=True)
+    password: Mapped[bytes] = mapped_column(BYTEA)
     is_active: Mapped[bool] = mapped_column(
         Boolean, default=True, server_default="true"
     )
