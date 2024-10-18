5f8ba5b6e288d8b276ffbb02a171f64abf0c80e3
zifrit <zifrit@list.ru>
2024-10-18 12:33:13 +0300

    fixed timezone created_at, updated_at and deleted_at field in base model

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -7,14 +7,14 @@ class Base(DeclarativeBase):
     __abstract__ = True
     id: Mapped[int] = mapped_column(primary_key=True)
     created_at: Mapped[datetime] = mapped_column(
-        DateTime(),
+        DateTime(timezone=True),
         server_default=func.now(),
         default=datetime.now(timezone.utc),
     )
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime(),
+        DateTime(timezone=True),
         server_default=func.now(),
         default=datetime.now(timezone.utc),
         onupdate=datetime.now(timezone.utc),
     )
-    deleted_at: Mapped[datetime | None] = mapped_column(DateTime())
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
